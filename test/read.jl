acb07a8047cd4539fca9172cfe1809533af3242d
Pere Mato <pere.mato@cern.ch>
2023-12-11 08:49:42 +0100

    synch

--- test/read.jl
+++ test/read.jl
@@ -7,34 +7,15 @@ f = "/Users/mato/Downloads/example_edm4hep2.root"
 
 reader = RootIO.Reader(f)
 events = RootIO.get(reader, "events")
-evt = events[1]
+evt = events[1];
 
-se_hits = RootIO.get(reader, evt, "SETCollection")
+set_hits = RootIO.get(reader, evt, "SETCollection")
 mcps =  RootIO.get(reader, evt, "MCParticle")
 
-
-for hit in set_hits:
-    mc = hit.getMCParticle()
-    print(f"Hit {hit.id().index} is related to MC {mc.id().index}")
-
-
-
-
-tree = LazyTree(f, "events", ("SITCollection","MCParticle"))
-
-layout_mcp = buildlayout(tree, "MCParticle", MCParticle)  # need the relation between the type and container name
-layout_hits = buildlayout(tree, "SITCollection", SimTrackerHit)
-
-df1 = DataFrame(getStructArray(tree[1], layout_mcp))
-df2 = DataFrame(getStructArray(tree[1], layout_hits))
-
-for evt in tree[1:10]
-    assignEDStore(getStructArray(evt, layout_mcp))
-    for p in getEDStore(MCParticle).objects
-        println("MCParticle $(p.index) with PDG=$(p.PDG) and momentum $(p.momentum) has $(length(p.daughters)) daughters")
-        #for d in p.daughters
-        #    println("   ---> $(d.index) with PDG=$(d.PDG) and momentum $(d.momentum)")
-        #end
-    end
+for hit in set_hits
+    println("Hit $(hit.index) is related to MCParticle $(hit.mcparticleidx)")
 end
 
+DataFrame(set_hits)
+DataFrame(mcps)[!,14:15]
+parents =  RootIO.get(reader, evt, "_MCParticle_parents")
