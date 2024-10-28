c871d225025ede1db4940afa27e12bc833c07237
Pere Mato <pere.mato@cern.ch>
2024-10-28 11:00:55 +0100

    Finish the transfer to JuliaHEP

--- docs/make.jl
+++ docs/make.jl
@@ -6,7 +6,7 @@ makedocs(;
     modules=[EDM4hep, EDM4hep.RootIO],
     format = Documenter.HTML(
         prettyurls = Base.get(ENV, "CI", nothing) == "true",
-        repolink="https://github.com/peremato/EDM4hep.jl",
+        repolink="https://github.com/JuliaHEP/EDM4hep.jl",
     ),
     pages=[
         "Introduction" => "index.md",
@@ -14,12 +14,12 @@ makedocs(;
         "Release Notes" => "release_notes.md",
     ],
     checkdocs=:exports,
-    repo="https://github.com/peremato/EDM4hep.jl/blob/{commit}{path}#L{line}",
+    repo="https://github.com/JuliaHEP/EDM4hep.jl/blob/{commit}{path}#L{line}",
     sitename="EDM4hep.jl",
     authors="Pere Mato",
 )
 
 deploydocs(;
-    repo="github.com/peremato/EDM4hep.jl",
+    repo="github.com/JuliaHEP/EDM4hep.jl",
     push_preview = true
 )
